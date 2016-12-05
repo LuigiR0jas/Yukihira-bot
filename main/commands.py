c8dc930aa50ef10d55c15e36a8501cb027d7c9d2
Luigi Rojas <xluigix98@gmail.com>
2016-12-05 01:52:52 -0400

    Fucking big update, bot can mantain threads of conversation
    No real threads, tho.

--- main/commands.py
+++ main/commands.py
@@ -5,107 +5,250 @@ import config
 import dbconnection
 import random
 import messageHandling
+from telepot.delegate import pave_event_space, per_chat_id, create_open
 
-#Manejar hilos de conversacion
-#Manejar ciclos repetitivos para asegurar la integridad de los datos ingresados
-#Crear funcion para manejar los queries de forma más sencilla
+TOKEN = config.apiKey
+restaurantName = "null"
+restaurantCategory = "null"
 
-def identifyCommand(command, id, username, firstname, type):
+def getUserState(id):
+    db = dbconnection.connect()
+    conn = dbconnection.conn
+    cursor = conn.cursor()
+    query0 = "SELECT state FROM users WHERE user_id = %s"
+    data0 = [id]
+    cursor.execute(query0, data0)
+    conn.commit()
+    records = cursor.fetchall()
+    if (len(records) == 0):
+        return 'null'
+    else:
+        return records[0]
+
+def saveUserState(id, state):
+    db = dbconnection.connect()
+    conn = dbconnection.conn
+    cursor = conn.cursor()
+    query0 = "UPDATE users SET state= %s WHERE user_id = %s"
+    data0 = (state,id)
+    cursor.execute(query0, data0)
+    conn.commit()
+
+def identifyCommandByState(state):
+    if (state >= 10 and state < 20):
+        print('Identifying command, state is ' + str(state))
+        return "/NewRestaurant"
+    elif (state >= 20 and state < 30):
+        print('Identifying command, state is ' + str(state))
+        return "/ChangeChef"
+    elif (state >= 30 and state < 40):
+        print('Identifying command, state is ' + str(state))
+        return "/RestDescription"
+    elif (state >= 40 and state < 50):
+        print('Identifying command, state is ' + str(state))
+        return "/EditMenu"
+    elif (state >= 50 and state < 60):
+        print('Identifying command, state is ' + str(state))
+        return "/EditRecipe"
+    elif (state >= 60 and state < 70):
+        print('Identifying command, state is ' + str(state))
+        return "/DishDescription"
+    elif (state >= 70 and state < 80):
+        print('Identifying command, state is ' + str(state))
+        return "/NewOrder"
+
+
+def identifyCommand(command, state, id, username, firstname, type, text):
     if (command == '/start'):
-        messageHandling.sendMessage(id, "Welcome my name is YukihiraBot. What can I do for you? \n If you want to create a new restaurant you can do it typing the /NewRestaurant command. If you want to make an order, please type /NewOrder If you want some more information use the /help command")
-        db = dbconnection.connect()
-        conn = dbconnection.conn
-        cursor = conn.cursor()
-        query0 = "SELECT user_id FROM users WHERE user_id = %s"
-        data0 = [id]
-        cursor.execute(query0, data0)
-        conn.commit()
-        records = cursor.fetchall()
-
-        if (len(records) == 0):
-            query2 = "INSERT INTO users (user_id, user_name, user_firstname) VALUES (%s, %s, %s)"
-            query3 = "INSERT INTO client (user_id) VALUES (%s)"
-            data2 = (id, username, firstname)
-            data3 = [id]
-            cursor.execute(query2, data2)
-            cursor.execute(query3, data3)
-            conn.commit()
-            print('User ' + firstname + '(ID: ' + str(id) + ', username: ' + username + ')' + ' is new, therefore has been added to the database')
-
+        start(id, username, firstname)
 
     elif (command == '/help'):
-        messageHandling.sendMessage(id, 'Command list:\n /NewRestaurant --- Creates a new Restaurant \n/ChangeChef --- Manage the chef of your restaurant \n/RestDescription --- Set a description for your restaurant \n/EditMenu --- Create or update your restaurant’s menu \n/EditRecipe --- Creates or edit a recipe for your menu \n/DishDescription --- Set a description for your dish \n /NewOrder --- Order the dish you want from any of the available restaurants')
+        help(id)
 
     elif (command == '/NewRestaurant'):
-        messageHandling.sendMessage(id, "What’s the name of your restaurant?")
-        #User sends the restaurant’s name
-        restaurantName = msg.txt; #Whatever the user sends
-        messageHandling.sendMessage(id, "Ok, type the number of the restaurant's specialty");
-        messageHandling.sendMessage(id, "1. Seafood \n2.Grill/Steakhouse \n3.Fastfood \n4.Vegetarian \n5.International \n6.Italian \n7.Chinese \n8.Mexican\n9.Other");
-        #User sends the restaurant’s category
-        db = dbconnection.connect()
-        conn = dbconnection.conn
-        cursor = conn.cursor()
-        query0 = "INSERT INTO restaurant (chef_id, owner_id, restaurant_demand, restaurant_name, restaurant_description, restaurant_adress) VALUES (%s, %s, %s, %s, %s, %s)"
-        data0 = (id, id, 0, restaurantName,'', '')
-        cursor.execute(query0, data0)
-        conn.commit()
-        #Chef: user_name (default)
-        messageHandling.sendMessage(id, 'Congratulations' + username + ', you are the new owner and main chef of' + restaurantName + '.If you want to create the menu for your restaurant, use the /EditMenu command, and if you are not the Chef of the restaurant type /ChangeChef to assign a new one')
+        NewRestaurant(state, id, username, firstname, type, text)
 
     elif (command == '/ChangeChef'):
-        db = dbconnection.connect()
-        conn = dbconnection.conn
-        cursor = conn.cursor()
-        query0 = "SELECT restaurant_name FROM restaurant WHERE owner_id = %s"
-        query1 = "SELECT chef_id FROM restaurant WHERE owner_id = %s"
-        data0 = [id]
-        data1 = [id]
-        cursor.execute(query0, data0)
-        conn.commit()
-        records = cursor.fetchall()
-        cursor.execute(query1, data1)
-        conn.commit()
-        records1 = cursor.fetchall()
-        restaurantName = records(0)
-        currentChefID = records1(0)
-        messageHandling.sendMessage(id, 'The current chef of ' + restaurantName+ ' is ' + currentChefID +', write the id of your new chef')
-        #User types chef’s username
-        newChefID = msg.txt
-        db = dbconnection.connect()
-        conn = dbconnection.conn
-        cursor = conn.cursor()
-        query0 = "UPDATE restaurant (chef_id) VALUES (%s) WHERE owner_id = %s"
-        data0 = (newChefID, id)
-        cursor.execute(query0, data0)
-        conn.commit()
-        messageHandling.sendMessage(id, 'The new chef of ' + restaurantName + ' is ' + NewChefID)
+        ChangeChef(state, id, username, firstname, type, text)
 
     elif (command == '/RestDescription'):
-        messageHandling.sendMessage(id,'Type your restaurant description (max 400 characters)')
-        #User sends description
-        restaurantDescription = msg.txt
+        RestDescription(state, id, username, firstname, type, text)
+
+    elif (command == '/EditMenu'):
+        EditMenu(state, id, username, firstname, type, text)
+
+    elif (command == '/EditRecipe'):
+        EditRecipe(state, id, username, firstname, type, text)
+
+    elif (command == '/DishDescription'):
+        DishDescription(state, id, username, firstname, type, text)
+
+    elif (command == '/NewOrder'):
+        NewOrder(state, id, username, firstname, type, text)
+
+    elif (command == '/cancel' and state != 0):
+        cancel(id)
+    elif (command == '/cancel' and state == 0):
+        messageHandling.sendMessage(id, "...Ok? I wasn't doing anything anyway .-.")
+    else:
+        if(type != 'group'):
+            messageHandling.sendMessage(id, "That's not a command of mine, pal. If you need help, send /help")
+
+def start(id, username, firstname):
+    messageHandling.sendMessage(id, "Welcome my name is YukihiraBot. What can I do for you? \n If you want to create a new restaurant you can do it typing the /NewRestaurant command. If you want to make an order, please type /NewOrder If you want some more information use the /help command")
+    db = dbconnection.connect()
+    conn = dbconnection.conn
+    cursor = conn.cursor()
+    query0 = "SELECT user_id FROM users WHERE user_id = %s"
+    data0 = [id]
+    cursor.execute(query0, data0)
+    conn.commit()
+    records = cursor.fetchall()
+
+    if (len(records) == 0):
+        query2 = "INSERT INTO users (user_id, user_name, user_firstname) VALUES (%s, %s, %s)"
+        query3 = "INSERT INTO client (user_id) VALUES (%s)"
+        data2 = (id, username, firstname)
+        data3 = [id]
+        cursor.execute(query2, data2)
+        cursor.execute(query3, data3)
+        conn.commit()
+        print('User ' + firstname + '(ID: ' + str(id) + ', username: ' + username + ')' + ' is new, therefore has been added to the database')
+
+def help(id):
+    messageHandling.sendMessage(id, 'Command list:\n /NewRestaurant --- Creates a new Restaurant \n/ChangeChef --- Manage the chef of your restaurant \n/RestDescription --- Set a description for your restaurant \n/EditMenu --- Create or update your restaurant’s menu \n/EditRecipe --- Creates or edit a recipe for your menu \n/DishDescription --- Set a description for your dish \n /NewOrder --- Order the dish you want from any of the available restaurants')
+
+def NewRestaurant(state, id, username, firstname, type, text):
+    if (state == 0):
+        messageHandling.sendMessage(id, "What’s the name of your restaurant?")
+        saveUserState(id, 11)
+    if (state == 11):
+        global restaurantName
+        restaurantName = text
+        messageHandling.sendMessage(id, "Ok, type the restaurant's specialty");
+        saveUserState(id, 12)
+        messageHandling.sendMessage(id, "1. Seafood \n2.Grill/Steakhouse \n3.Fastfood \n4.Vegetarian \n5.International \n6.Italian \n7.Chinese \n8.Mexican\n9.Other");
+    if (state == 12):
+        global restaurantCategory
+        restaurantCategory = text
         db = dbconnection.connect()
         conn = dbconnection.conn
         cursor = conn.cursor()
-        query0 = "UPDATE restaurant (restaurant_description) VALUES (%s) WHERE owner_id = %s"
-        data0 = (restaurantDescription, id)
+        query0 = "INSERT INTO chef (user_id, chef_id) VALUES (%s, %s)"
+        data0 = (id, id)
+        query1 = "INSERT INTO owner (user_id, owner_id) VALUES (%s, %s)"
+        data1 = (id, id)
+        query2 = "INSERT INTO restaurant (chef_id, user_id, owner_id, restaurant_demand, restaurant_name, restaurant_description, restaurant_address, restaurant_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        data2 = (id, id, id, 0, restaurantName, "", "", restaurantCategory)
         cursor.execute(query0, data0)
+        cursor.execute(query1, data1)
+        cursor.execute(query2, data2)
         conn.commit()
-        messageHandling.sendMessage(id,'“The description of (restaurant_name) has been changed”')
+        messageHandling.sendMessage(id, 'Congratulations ' + username + ', you are the new owner and main chef of ' + restaurantName + '. If you want to create the menu for your restaurant, use the /EditMenu command, and if you are not the Chef of the restaurant type /ChangeChef to assign a new one')
+        saveUserState(id, 0)
 
-    elif (command == '/EditMenu'):
-        messageHandling.sendMessage(id,)
+def ChangeChef(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside ChangeChef function, on state 20, please send me another message to get out of here")
+        saveUserState(id, 21)
+    if (state == 21):
+        messageHandling.sendMessage(id, "You're inside ChangeChef  function, on state 21. Another one and you'll be free!")
+        saveUserState(id, 22)
+    if (state == 22):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in ChangeChef function, on state 22! No matter what you do, you can't leave this place")
 
-    elif (command == 'EditRecipe'):
-        messageHandling.sendMessage(id,)
+    # db = dbconnection.connect()
+    # conn = dbconnection.conn
+    # cursor = conn.cursor()
+    # query0 = "SELECT restaurant_name FROM restaurant WHERE owner_id = %s"
+    # query1 = "SELECT chef_id FROM restaurant WHERE owner_id = %s"
+    # data0 = [id]
+    # data1 = [id]
+    # cursor.execute(query0, data0)
+    # conn.commit()
+    # records = cursor.fetchall()
+    # cursor.execute(query1, data1)
+    # conn.commit()
+    # records1 = cursor.fetchall()
+    # restaurantName = records(0)
+    # currentChefID = records1(0)
+    # messageHandling.sendMessage(id, 'The current chef of ' + restaurantName+ ' is ' + currentChefID +', write the id of your new chef')
+    # #User types chef’s username
+    # newChefID = msg.txt
+    # db = dbconnection.connect()
+    # conn = dbconnection.conn
+    # cursor = conn.cursor()
+    # query0 = "UPDATE restaurant (chef_id) VALUES (%s) WHERE owner_id = %s"
+    # data0 = (newChefID, id)
+    # cursor.execute(query0, data0)
+    # conn.commit()
+    # messageHandling.sendMessage(id, 'The new chef of ' + restaurantName + ' is ' + NewChefID)
 
-    elif (command == '/DishDescription'):
-        messageHandling.sendMessage(id,)
+def RestDescription(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside RestDescription  function, on state 30, please send me another message to get out of here")
+        saveUserState(id, 31)
+    if (state == 31):
+        messageHandling.sendMessage(id, "You're inside RestDescription function, on state 31. Another one and you'll be free!")
+        saveUserState(id, 32)
+    if (state == 32):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in RestDescription  function, on state 32! No matter what you do, you can't leave this place")
 
-    elif (command == '/NewOrder'):
-        messageHandling.sendMessage(id,)
+    # messageHandling.sendMessage(id,'Type your restaurant description (max 400 characters)')
+    # #User sends description
+    # restaurantDescription = msg.txt
+    # db = dbconnection.connect()
+    # conn = dbconnection.conn
+    # cursor = conn.cursor()
+    # query0 = "UPDATE restaurant (restaurant_description) VALUES (%s) WHERE owner_id = %s"
+    # data0 = (restaurantDescription, id)
+    # cursor.execute(query0, data0)
+    # conn.commit()
+    # messageHandling.sendMessage(id,'“The description of (restaurant_name) has been changed”')
 
-    else:
-        if(type != 'group'):
-            messageHandling.sendMessage(id, "If you need help, send /help")
+def EditMenu(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside EditMenu function, on state 40, please send me another message to get out of here")
+        saveUserState(id, 41)
+    if (state == 41):
+        messageHandling.sendMessage(id, "You're inside EditMenu function, on state 41. Another one and you'll be free!")
+        saveUserState(id, 42)
+    if (state == 42):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in EditMenu function, on state 42! No matter what you do, you can't leave this place")
+
+
+def EditRecipe(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside EditRecipe function, on state 50, please send me another message to get out of here")
+        saveUserState(id, 51)
+    if (state == 51):
+        messageHandling.sendMessage(id, "You're inside EditRecipe function, on state 51. Another one and you'll be free!")
+        saveUserState(id, 52)
+    if (state == 52):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in EditRecipe function, on state 52! No matter what you do, you can't leave this place")
+
+
+def DishDescription(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside DishDescription function, on state 60, please send me another message to get out of here")
+        saveUserState(id, 61)
+    if (state == 61):
+        messageHandling.sendMessage(id, "You're inside DishDescription function, on state 61. Another one and you'll be free!")
+        saveUserState(id, 62)
+    if (state == 62):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in DishDescription function, on state 62! No matter what you do, you can't leave this place")
+
+
+def NewOrder(state, id, username, firstname, type):
+    if (state == 0):
+        messageHandling.sendMessage(id, "You're inside NewOrder function, on state 70, please send me another message to get out of here")
+        saveUserState(id, 71)
+    if (state == 71):
+        messageHandling.sendMessage(id, "You're inside NewOrder function, on state 71. Another one and you'll be free!")
+        saveUserState(id, 72)
+    if (state == 72):
+        messageHandling.sendMessage(id, "hahaha you're now trapped in NewOrder function, on state 72! No matter what you do, you can't leave this place")
+
+def cancel(id):
+    saveUserState(id, 0)
+    messageHandling.sendMessage(id, "Oww :C. You found my weakness! Very well, your state is now 0 again")
--- main/messageHandling.py
+++ main/messageHandling.py
@@ -26,18 +26,48 @@ def handle(msg):
     else:
         username = 'N/A'
     print('·' + firstname + ' (from ' + type + ' chat), at time ' + date + ', says:\n' + text + '\n')
-    analize(msg, text, id, username, firstname, type)
+    analisys = analize(msg, text, id, type)
+
+    if (isinstance(analisys, str)):
+        state = commands.getUserState(id)
+        commands.identifyCommand(analisys, state[0], id, username, firstname, type, text)
+    elif(analisys == 1):
+        state = commands.getUserState(id)
+        # print("In message hanlder: " + str(state[0]))
+        command = commands.identifyCommandByState(state[0])
+        # print('Again in message handler: ' + command)
+        commands.identifyCommand(command, state[0], id, username, firstname, type, text)
+    elif(analisys == 0):
+        state = commands.getUserState(id)
+        theBot.sendMessage(id, 'I can only understand you via commands. If you need help, send /help. It was nothing!')
+        # print('type is ' + type + ' and state is ' + str(state[0]) + '. Analisys is: ' )
+        # print(analisys)
 
 
-def analize(msg, text, id, username, firstname, type):
-    if('entities' in msg):
+def analize(msg, text, id, type):
+    state = commands.getUserState(id)
+    if('entities' in msg and state[0] == 0):
         textArray = text.split(" ")
         command = textArray[0]
         print('It\'s a command, and it says: ' + command + '\n')
-        commands.identifyCommand(command, id, username, firstname, type)
+        return command
+    elif('entities' in msg and state == 'null'):
+        return '/start'
+    elif('entities' in msg and state[0] != 0 and type != 'group'):
+        textArray = text.split(" ")
+        command = textArray[0]
+        if (command == "/cancel"):
+            print('It\'s a cancel command')
+            return command
+        else:
+            return 1
     else:
-        if(type != 'group'):
-            theBot.sendMessage(id, 'I can only understand you via commands. If you need help, send /help. It was nothing!')
+        if(type != 'group' and state[0] != 0):
+            print(1)
+            return 1
+        elif(type != 'group' and state[0] == 0):
+            print(0)
+            return 0
 
 def sendMessage(id, text):
     theBot.sendMessage(id, text)
